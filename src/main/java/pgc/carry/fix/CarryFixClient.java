286d3ccd802a90acc04b3c8a23b0a59548e5e034
PGC <121595426+PGC48@users.noreply.github.com>
2026-04-30 01:51:20 +0700

    Clean up initialization comment in CarryFixClient
    Removed unnecessary comment regarding Mixin usage.

--- src/main/java/pgc/carry/fix/CarryFixClient.java
+++ src/main/java/pgc/carry/fix/CarryFixClient.java
@@ -5,7 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class CarryFixClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // ทุกอย่างทำงานผ่าน Mixin จึงไม่ต้องเขียนคำสั่งเริ่มต้นที่นี่
         System.out.println("[CarryFix] Initialized Shader Compatible Renderer for Carry On");
     }
 }
